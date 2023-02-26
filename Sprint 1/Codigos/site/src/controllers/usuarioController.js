e72720d9f645da7dcc6b0e69f4da71289cc4f4ca
NathanBin <nathan.lopes@sptech.school>
2023-02-26 18:09:30 -0300

    Função de cadastro/login e pequena atualização na tela de perfil

--- Sprint 1/Codigos/site/src/controllers/usuarioController.js
+++ Sprint 1/Codigos/site/src/controllers/usuarioController.js
@@ -2,6 +2,33 @@ var usuarioModel = require("../models/usuarioModel");
 
 var sessoes = [];
 
+function cadastrar(req, res){
+    const nome = req.body.nome;
+    const cnpj = req.body.cnpj;
+    const email = req.body.email;
+    const senha = req.body.senha;
+
+    usuarioModel.cadastrar(nome, cnpj, email, senha)
+        .then(
+            function(resultado){
+                res.json(resultado)
+            }
+        )
+}
+
+function autenticar(req, res){
+    const email = req.body.email;
+    const senha = req.body.senha;
+
+    usuarioModel.autenticar(email, senha)
+        .then(
+            function(resultado){
+                res.json(resultado)
+            }
+        )
+}
+
 module.exports = {
-   
+   cadastrar,
+   autenticar
 }
